d21cd979f8edf071e8d1decadd5b29e111ecb33d
nalandyaf <nalandyarizky@gmail.com>
2023-07-07 13:51:09 +0700

    create usecase get list movie

---
+++ app/src/main/java/com/test/movie/domain/usecase/GetList.kt
@@ -0,0 +1,19 @@
+package com.test.movie.domain.usecase
+
+import com.test.movie.domain.model.MovieList
+import com.test.movie.domain.repository.MovieRepository
+import com.test.movie.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+import javax.inject.Inject
+
+class GetList @Inject constructor(private val movieRepository: MovieRepository) {
+    operator fun invoke(
+        listId: String, page: Int, region: String? = null
+    ): Flow<Resource<MovieList>> = flow {
+        emit(
+            movieRepository.getMovieList(listId, page, region)
+        )
+    }
+}
