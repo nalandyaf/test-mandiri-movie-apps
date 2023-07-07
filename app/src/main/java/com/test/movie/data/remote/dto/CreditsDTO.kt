0022a2d824a166459b078f27fcd46d9bcccb10c3
nalandyaf <nalandyarizky@gmail.com>
2023-07-07 11:11:16 +0700

    generate all dto from json

---
+++ app/src/main/java/com/test/movie/data/remote/dto/CreditsDTO.kt
@@ -0,0 +1,10 @@
+package com.test.movie.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CreditsDTO(
+    @SerializedName("cast") val cast: List<PersonDTO>,
+    @SerializedName("crew") val crew: List<PersonDTO>,
+    @SerializedName("guest_stars") val guestStars: List<PersonDTO>?
+)
